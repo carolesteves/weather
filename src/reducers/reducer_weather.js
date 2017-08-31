955c6f05071f1c841c936c06d7da5a7cc2092ef7
carol_esteves <caroline.g.esteves@gmail.com>
2017-08-31 15:39:20 -0300

    Import FETCH_WEATHER to reducer_weather

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    switch (action.type) {
+    case FETCH_WEATHER:
+        // return state.concat([action.payload.data]);
+        // the line below means the sabe as the above code
+        return [ action.payload.data, ...state ];
+    }
     return state;
 }
