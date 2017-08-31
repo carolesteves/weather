c716801dba473b188552dd28d8f59eecf70d3864
carol_esteves <caroline.g.esteves@gmail.com>
2017-08-31 14:29:17 -0300

    Add fetchWeather function to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY ='14b174df08d8020c0b03aeffc4b07f2e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+
+    return {
+        type: FETCH_WEATHER
+    };
+}
