04f1bc7a97a5c54fb7003f1e961b95318dbdfb3f
carol_esteves <caroline.g.esteves@gmail.com>
2017-09-01 10:28:29 -0300

    Update chart colors

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -18,9 +18,9 @@ class WeatherList extends Component {
         return (
             <tr key={name}>
                 <td><GoogleMap lon={lon} lat={lat} /></td>
-                <td><Chart data={temps} color="orange" units="°C" /></td>
-                <td><Chart data={pressures} color="green" units="hPa" /></td>
-                <td><Chart data={humidities} color="blue" units="%" /></td>
+                <td><Chart data={temps} color="#ffc62d" units="°C" /></td>
+                <td><Chart data={pressures} color="#8fda2e" units="hPa" /></td>
+                <td><Chart data={humidities} color="#00b4ff" units="%" /></td>
             </tr>
         );
     }
