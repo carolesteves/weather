17b44ae76bb975cbec9cee8e8f4f50d4472b3d05
carol_esteves <caroline.g.esteves@gmail.com>
2017-09-01 09:34:53 -0300

    Adding units to charts and converting Kelvin to Celsius

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,16 +5,17 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
+        // temperature comes in Kelvin, below it is converted to celsius
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td><Chart data={temps} color="orange" /></td>
-                <td><Chart data={pressures} color="green" /></td>
-                <td><Chart data={humidities} color="blue" /></td>
+                <td><Chart data={temps} color="orange" units="°C" /></td>
+                <td><Chart data={pressures} color="green" units="hPa" /></td>
+                <td><Chart data={humidities} color="blue" units="%" /></td>
             </tr>
         );
     }
@@ -25,9 +26,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (°C)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
